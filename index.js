b2b44c2526eea815c4d861ed9f2b2c253577ade7
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2023-06-12 12:17:46 +0600

    'delete'

--- index.js
+++ index.js
@@ -63,13 +63,13 @@ async function run() {
         const result = await enrollCollection.insertOne(enrollClass);
         res.send(result)
       })
-    //    // Delete
-    // app.delete('/enroll/:id', async(req,res)=>{
-    //   const id= req.params.id;
-    //   const query= {_id:new ObjectId(id)};
-    //   const result= await enrollCollection.deleteOne(query);
-    //   res.send(result);
-    // })
+       // Delete
+    app.delete('/enroll/:id', async(req,res)=>{
+      const id= req.params.id;
+      const query= {_id:new ObjectId(id)};
+      const result= await enrollCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     //.............................
