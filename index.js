488b487f4ec89bc19c707e6d6b6d52fbd3a81f14
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2023-06-12 14:04:39 +0600

    user

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 const cors = require('cors')
 require('dotenv').config();
 app.use(express.json())
@@ -27,49 +27,69 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+
     const instructorCollection = client.db("sportsDB").collection("instructor");
 
+    // class
     const classCollection = client.db("sportsDB").collection("classes");
 
     // Enrol
     const enrollCollection = client.db("sportsDB").collection("enroll");
 
-    app.get('/instructor', async(req,res)=>{
+    // User
+    const userCollection = client.db("sportsDB").collection("user");
+
+    app.get('/instructor', async (req, res) => {
       const result = await instructorCollection.find().toArray();
       res.send(result);
     })
 
-    app.get('/classes', async(req,res)=>{
+    app.get('/classes', async (req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
     })
     // ....................
-      // Get class
-      app.get('/enroll', async(req,res)=>{
-        const email= req.query.email;
-        console.log(email);
-        if(!email){
-          res.send([]);
-        }
-        const query={email:email};
-        const result = await enrollCollection.find(query).toArray();
-        res.send(result);
-      })
-  
-    // Post
-      app.post('/enroll', async (req, res) => {
-        const enrollClass = req.body;
-        // console.log(enrollClass);
-        const result = await enrollCollection.insertOne(enrollClass);
-        res.send(result)
-      })
+    // Get class
+    app.get('/enroll', async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await enrollCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // Post enroll
+    app.post('/enroll', async (req, res) => {
+      const enrollClass = req.body;
+      // console.log(enrollClass);
+      const result = await enrollCollection.insertOne(enrollClass);
+      res.send(result)
+    })
+
+    // post user
+    app.post('/user', async (req, res) => {
+      const userClass = req.body;
+      console.log(userClass);
+      const query ={email:userClass.email};
+      const alreadyUser= await userCollection.findOne(query);
+      if (alreadyUser){
+        return res.send({message:'This user already have'})
+      }
+
+      const result = await userCollection.insertOne(userClass);
+      res.send(result)
+    })
+
+
     // Delete
-    app.delete('/enroll/:id', async(req,res)=>{
-      const id= req.params.id;
+    app.delete('/enroll/:id', async (req, res) => {
+      const id = req.params.id;
       console.log(id);
-      const query= {_id:new ObjectId(id)};
-      const result= await enrollCollection.deleteOne(query);
+      const query = { _id: new ObjectId(id) };
+      const result = await enrollCollection.deleteOne(query);
       res.send(result);
     })
 
@@ -89,9 +109,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("Project-12");
-  })
+  res.send("Project-12");
+})
 
 app.listen(port, () => {
-    console.log(`port is: ${port}`)
-  })
+  console.log(`port is: ${port}`)
+})
